91427f1bf4b7ea0daa9434f46a875e0200adb00f
Yoshiaki NAGASAWA <ykk.26camellia@gmail.com>
2017-01-01 17:39:48 +0900

    Add FizzBuzz script by Python

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+count = 1
+
+while(count >= 100):
+    if(count % 15 == 0):
+        print("FizzBuzz")
+    elif(count % 3 == 0):
+        print("Fizz")
+    elif(count % 5 == 0):
+        print("Buzz")
+    else:
+        print(count)
+    x += 1
